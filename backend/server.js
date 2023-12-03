6cc0dee7fde3b3ef80956179b348bdfd9ac24ce0
Brijsunil2 <33044885+Brijsunil2@users.noreply.github.com>
2023-12-03 18:16:46 -0500

    Initalize MongoDB server

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,11 @@ import dotenv from "dotenv";
 dotenv.config();
 import cookieParser from "cookie-parser";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import connectDB from "./config/dbConfig.js";
 import userRoutes from "./routes/userRoutes.js";
 
 const port = process.env.PORT || 5000;
+connectDB();
 
 const app = express();
 app.use(express.json());
@@ -15,7 +17,7 @@ app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
 
-app.get("/", (req, res) => res.send("Server Online"));
+app.get("/", (req, res) => res.send("Portal Server Online"));
 
 app.use(notFound);
 app.use(errorHandler);
