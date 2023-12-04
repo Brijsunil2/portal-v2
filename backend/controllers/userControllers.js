a433d0493fd429249f56718b9493e40da4d12857
Brijsunil2 <33044885+Brijsunil2@users.noreply.github.com>
2023-12-04 14:12:22 -0500

    Implement basic user controller for requests sent by client

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,23 +1,75 @@
 import asyncHandler from "express-async-handler";
+import generateToken from "../utils/generateToken.js";
+import User from "../models/userModel.js";
+
+const sendUserData = (res, user) => {
+  res.status(201).json({
+    _id: user.id,
+    firstname: user.firstname,
+    lastname: user.lastname,
+    username: user.username,
+    email: user.email,
+  });
+};
 
 const authUser = asyncHandler(async (req, res) => {
-  console.log("Auth User");
-  res.status(200).json({msg: "Auth User"});
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id);
+    sendUserData(res, user);
+  } else {
+    res.status(401);
+    throw new Error("Invalid email or password");
+  }
 });
 
 const registerUser = asyncHandler(async (req, res) => {
-  console.log("Register User");
-  res.status(200).json({msg: "Register User"});
+  const { firstname, lastname, username, email, password } = req.body;
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  const user = await User.create({
+    firstname,
+    lastname,
+    username,
+    email,
+    password,
+  });
+
+  if (user) {
+    generateToken(res, user._id);
+    sendUserData(res, user);
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
 });
 
 const logoutUser = asyncHandler(async (req, res) => {
-  console.log("Logout User");
-  res.status(200).json({msg: "Logout User"});
+  res.cookie("userjwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+
+  res.status(200).json({ message: "User logged out" });
 });
 
 const getUser = asyncHandler(async (req, res) => {
-  console.log("Get User");
-  res.status(200).json({msg: "Get User"});
+  const user = {
+    _id: req.user.id,
+    firstname: req.user.firstname,
+    lastname: req.user.lastname,
+    username: req.user.username,
+    email: req.user.email,
+  };
+
+  res.status(200).json(user);
 });
 
 export { authUser, registerUser, logoutUser, getUser };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -6,10 +6,11 @@ import {
   logoutUser,
   getUser,
 } from "../controllers/userControllers.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 router.post("/", registerUser);
 router.post("/auth", authUser);
 router.post("/logout", logoutUser);
-router.get("/user", getUser);
+router.route("/user").get(protect, getUser);
 
 export default router;
