c40e66603b62e1e88c5d3b4077e4ba1d08fd5fff
Brijsunil2 <33044885+Brijsunil2@users.noreply.github.com>
2023-12-20 20:43:41 -0500

    Added placeholder apps

--- frontend/src/components/AppSection/AppSection.jsx
+++ frontend/src/components/AppSection/AppSection.jsx
@@ -2,6 +2,8 @@ import "./AppSection.css";
 import { Col, Container, Row } from "react-bootstrap";
 import { MdForum } from "react-icons/md";
 import { BsCalendarEvent } from "react-icons/bs";
+import { FaBoxArchive } from "react-icons/fa6";
+import { IoIosCalculator } from "react-icons/io";
 import AppEntry from "./AppEntry";
 
 const AppSection = () => {
@@ -18,6 +20,18 @@ const AppSection = () => {
             <AppEntry appName="Calendar" appIcon={<BsCalendarEvent />} appDesc="Coming soon"/>
             </Col>
             <Col className="my-2" md>
+            <AppEntry appName="Storage" appIcon={<FaBoxArchive />} appDesc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."/>
+            </Col>
+          </Row>
+          <Row>
+            <Col className="my-2" md>
+            <AppEntry appName="Calculator" appIcon={<IoIosCalculator />} appDesc="Feugiat scelerisque varius morbi enim. Aliquam vestibulum morbi blandit cursus risus at ultrices mi."/>
+            </Col>
+            <Col className="my-2" md>
+
+            </Col>
+            <Col className="my-2" md>
+              
             </Col>
           </Row>
         </Container>
