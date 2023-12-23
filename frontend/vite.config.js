2c33dc93bf14f1bc5ec4c5f241ce5f04efda9018
Brijsunil2 <33044885+Brijsunil2@users.noreply.github.com>
2023-12-23 18:15:32 -0500

    Getting ready to Deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,27 +1,5 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     port: 3000,
-//     proxy: {
-//       "/api": {
-//         target: "http://localhost:5000",
-//         changeOrigin: true,
-//       },
-//     },
-//     define: {
-//       'process.env': env
-//     },
-//   },
-// });
-
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
   return {
@@ -30,7 +8,7 @@ export default defineConfig(({ mode }) => {
       port: 3000,
       proxy: {
         "/api": {
-          target: "http://localhost:5000",
+          target: "https://portal-demo.onrender.com",
           changeOrigin: true,
         },
       },
