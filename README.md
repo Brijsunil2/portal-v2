be9423a7e9ec7be29c6b4be2717ea0b7c4ecb7e5
Brijsunil2 <33044885+Brijsunil2@users.noreply.github.com>
2024-01-03 21:00:14 -0500

    Initial portal-v2 commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# portal-v2
-This is a newer version of the portal app. Since the old portal app also includes the forums, I thought this was bad designs. The purpose of this app is to divide the login and homepage section and have separate repository apps hosted on this site. 
+# portal-release
+An application that allows users to login to gain access to a dashboard of apps.
--- frontend/src/components/AppSection/AppSection.jsx
+++ frontend/src/components/AppSection/AppSection.jsx
@@ -1,9 +1,6 @@
 import "./AppSection.css";
 import { Col, Container, Row } from "react-bootstrap";
 import { MdForum } from "react-icons/md";
-import { BsCalendarEvent } from "react-icons/bs";
-import { FaBoxArchive } from "react-icons/fa6";
-import { IoIosCalculator } from "react-icons/io";
 import AppEntry from "./AppEntry";
 
 const AppSection = () => {
@@ -14,25 +11,14 @@ const AppSection = () => {
         <Container className="appentries-container">
           <Row>
             <Col className="my-2" md>
-            <AppEntry appName="Forums" appIcon={<MdForum />} appDesc="A platform where users can create forum groups to communicate, ask questions or share any concerns."/>
+              <AppEntry
+                appName="Forums"
+                appIcon={<MdForum />}
+                appDesc="A platform where users can create forum groups to communicate, ask questions or share any concerns."
+              />
             </Col>
-            <Col className="my-2" md>
-            <AppEntry appName="Calendar" appIcon={<BsCalendarEvent />} appDesc="Coming soon"/>
-            </Col>
-            <Col className="my-2" md>
-            <AppEntry appName="Storage" appIcon={<FaBoxArchive />} appDesc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."/>
-            </Col>
-          </Row>
-          <Row>
-            <Col className="my-2" md>
-            <AppEntry appName="Calculator" appIcon={<IoIosCalculator />} appDesc="Feugiat scelerisque varius morbi enim. Aliquam vestibulum morbi blandit cursus risus at ultrices mi."/>
-            </Col>
-            <Col className="my-2" md>
-
-            </Col>
-            <Col className="my-2" md>
-              
-            </Col>
+            <Col className="my-2" md></Col>
+            <Col className="my-2" md></Col>
           </Row>
         </Container>
       </Container>
